a9cb514f934c3b35f8b00bc992bd8431aeb08ee4
Daniel Varón <daniel.varon@mail.escuelaign.edu.co>
2021-04-02 16:18:23 -0500

    PARTE 3

--- src/main/java/edu/eci/arsw/collabpaint/PaintController.java
+++ src/main/java/edu/eci/arsw/collabpaint/PaintController.java
@@ -1,6 +1,7 @@
 package edu.eci.arsw.collabpaint;
 
 import edu.eci.arsw.collabpaint.model.Point;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PaintController {
 
-    @MessageMapping("/hello")
-    @SendTo("/topic/newpoint")
-    public String greeting(Point pt) throws Exception {
+    @MessageMapping("/newpoint.{id}")
+    @SendTo("/topic/newpoint.{id}")
+    public String greeting(Point pt, @DestinationVariable String id) throws Exception {
         System.out.println("Nuevo punto recibido.");
         Thread.sleep(1000); // simulated delay
         return ("Hello");
